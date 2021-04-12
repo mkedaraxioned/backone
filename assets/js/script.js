4dfe78536e1147c403318d4886003128e2974f4f
mkedaraxioned <kedarm@prdxn.com>
2021-04-12 12:26:13 +0530

    filter and back to top working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,70 @@
 /* Author: 
 
 */
+var list_img = document.querySelectorAll(".list-img > img");
+var current_img=document.querySelectorAll(".list-img > img:hover+ a > .portfolio-inner");
+for(var i=0;i<list_img.length;i++)
+list_img[i].addEventListener("mouseenter",hoverimage);
+function hoverimage(event) {
+    console.log("Message sent"+event.target.parentNode.nextSibling.childNodes);
+    let fig=event.target.parentNode;
+    // fig.
+    console.log(next_divs);
+    for(var i=0;i<next_divs.length;i++)
+    next_divs[i].style.display="block";
+    
+    // current_img.style.display="block";
+}
+
+// filter
+
+var filter=document.querySelector(".portfolio-filters");
+filter.addEventListener("click", function(e) {
+    if(e.target && e.target.nodeName == "SPAN") {
+    var x = document.querySelectorAll(".tabs");
+    for (var i = 0; i < x.length; i++) {
+      if(x[i].classList.contains("bg-green"))
+      {
+      x[i].classList.remove("bg-green");        
+      x[i].classList.add("bg-darkgreen");
+      }
+    }
+    var image_li=document.querySelectorAll(".portfolio-item");
+    for (var j = 0; j < image_li.length; j++) {
+      if(image_li[j].classList.contains("show-img"))
+      image_li[j].classList.remove("show-img");
+      image_li[j].classList.add("hide-img");
+    }
+    var classname_sel=e.target.className;
+    for (var j = 0; j < image_li.length; j++) {
+        if(classname_sel=="all"){
+          if(image_li[j].classList.contains("hide-img")){
+            image_li[j].classList.remove("hide-img");
+            image_li[j].classList.add("show-img");
+          }
+        }
+        else if(image_li[j].classList.contains(classname_sel)) {
+           if(image_li[j].classList.contains("hide-img")){
+            image_li[j].classList.remove("hide-img");
+            image_li[j].classList.add("show-img");
+          }
+        }
+    }
+    if(e.target.parentNode.classList.contains("bg-darkgreen"))
+      {
+        e.target.parentNode.classList.remove("bg-darkgreen");        
+        e.target.parentNode.classList.add("bg-green");
+      }
+    }  
+});
+
+var back_to_top =document.querySelector(".back-to-top");
+back_to_top.addEventListener("click",move_to_top);
+// back to top
+function move_to_top() {
+    document.documentElement.scrollTop = 0; 
+    document.body.scrollTop = 0;
+  }
 
 
 
